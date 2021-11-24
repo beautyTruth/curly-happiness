c59bc30b4e0d1179669b4dc62e5dc7a93b583cf7
Phil <happy.melodist@gmail.com>
2021-11-23 21:29:12 -0600

    add event listener to the cards and begin logic for the flipCard function

--- app.js
+++ app.js
@@ -1,67 +1,93 @@
-const cards = document.querySelectorAll(".memory-card");
+// const cards = document.querySelectorAll(".memory-card");
 
-let cardIsFlipped = false;
-let lockBoard = false;
-let firstCard, secondCard;
+// let cardIsFlipped = false;
+// let lockBoard = false;
+// let firstCard, secondCard;
 
-function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
+// function flipCard() {
+//   if (lockBoard) return;
+//   if (this === firstCard) return;
 
-  // this.classList.toggle("flip");
-  this.classList.add("flip");
+//   // this.classList.toggle("flip");
+//   this.classList.add("flip");
 
-  if (!cardIsFlipped) {
-    // first click -> first card
-    cardIsFlipped = true;
-    firstCard = this;
-    return;
-  }
-  // second click -> second card
-  secondCard = this;
+//   if (!cardIsFlipped) {
+//     // first click -> first card
+//     cardIsFlipped = true;
+//     firstCard = this;
+//     return;
+//   }
+//   // second click -> second card
+//   secondCard = this;
 
-  checkForMatch();
-}
+//   checkForMatch();
+// }
 
-function checkForMatch() {
-  let isMatched = firstCard.dataset.name === secondCard.dataset.name;
-  isMatched ? disableCards() : unFlipCards();
-}
+// function checkForMatch() {
+//   let isMatched = firstCard.dataset.name === secondCard.dataset.name;
+//   isMatched ? disableCards() : unFlipCards();
+// }
 
-function disableCards() {
-  firstCard.removeEventListener("click", flipCard);
-  secondCard.removeEventListener("click", flipCard);
+// function disableCards() {
+//   firstCard.removeEventListener("click", flipCard);
+//   secondCard.removeEventListener("click", flipCard);
 
-  resetBoard();
-}
+//   resetBoard();
+// }
 
-function unFlipCards() {
-  lockBoard = true;
+// function unFlipCards() {
+//   lockBoard = true;
 
-  setTimeout(() => {
-    firstCard.classList.remove("flip");
-    secondCard.classList.remove("flip");
-    resetBoard();
-  }, 1500);
-}
+//   setTimeout(() => {
+//     firstCard.classList.remove("flip");
+//     secondCard.classList.remove("flip");
+//     resetBoard();
+//   }, 1500);
+// }
 
-function resetBoard() {
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-  [cardIsFlipped, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
-}
+// function resetBoard() {
+//   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+//   [cardIsFlipped, lockBoard] = [false, false];
+//   [firstCard, secondCard] = [null, null];
+// }
 
-// IIFE -> Immediately Invoked Function Expression => function is called immediately after its definition
-(function shuffle() {
-  cards.forEach(function (card) {
-    let randomPositions = Math.floor(Math.random() * 12);
-    card.style.order = randomPositions;
-  });
-})();
+// // IIFE -> Immediately Invoked Function Expression => function is called immediately after its definition
+// (function shuffle() {
+//   cards.forEach(function (card) {
+//     let randomPositions = Math.floor(Math.random() * 12);
+//     card.style.order = randomPositions;
+//   });
+// })();
 
-cards.forEach(function (card) {
-  card.addEventListener("click", flipCard);
-});
+// cards.forEach(function (card) {
+//   card.addEventListener("click", flipCard);
+// });
 
 // fall down 7 times, get up 8
 // value yourself
+
+/*
+my code below
+ */
+
+const cards = document.querySelectorAll(".memory-card");
+
+let cardIsFlipped = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  // console.log(this);
+  this.classList.toggle("flip");
+
+  if (!cardIsFlipped) {
+    cardIsFlipped = true;
+    firstCard = this;
+  } else {
+    cardIsFlipped = false;
+    secondCard = this;
+  }
+}
+
+cards.forEach((card) => {
+  card.addEventListener("click", flipCard);
+});
